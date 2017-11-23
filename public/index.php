827b00f59b5a6f7869434ea83b52695b90d0d1ef
Aji Kamaludin <aji19kamaludin@gmail.com>
2017-11-23 16:32:23 +0700

    error request header

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Header: *');
+header('Access-Control-Allow-Header: Authorization');
 header('Access-Control-Request-Header: *');
 
 require __DIR__ . '/../vendor/autoload.php';
